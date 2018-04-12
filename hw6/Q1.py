dcb1645b1287712aae3039f973815427b22e4a5b
zhongturtle <zhongturtle@gmail.com>
2018-04-12 21:34:42 +0800

    upload hw6

---
+++ hw6/Q1.py
@@ -0,0 +1,32 @@
+n = 5
+graph = ""
+
+for i in range(0 , n):
+    line = ""
+    #for j in range(0 , n - i -1):
+    #    line = line + " "
+    line = line + " " * (n - i -1)
+    line = line + "*"
+    #for j in range(0 , 2 * i - 1):
+    #    line = line + " "
+    line = line + " " * (2 * i -1)
+
+    line = line + "*\n"
+    graph = graph + line
+
+for i in range(0 , n -1):
+    line = ""
+    #for j in range(0 , i + 1):
+    #    line = line + " "
+    line = line + " " * (i + 1)
+    line = line + "*"
+    #for j in range(0 , 2*(n - i -2) - 1):
+    #    line = line + " "
+    line = line + " " * (2*(n - i -2) - 1)
+    line = line + "*\n"
+    graph = graph + line
+
+graph = graph.replace("**" ,"*")
+#print(graph.count("**"))
+
+print(graph)
---
+++ hw6/Q2.py
@@ -0,0 +1,20 @@
+n = 7
+graph = ""
+
+for i in range(0 , n - 1):
+    line = ""
+    #for j in range(0 , n - i -1):
+    #    line = line + " "
+    line = line + " " * (n - i -1)
+    line = line + "*"
+    #for j in range(0 , 2 * i - 1):
+    #    line = line + " "
+    line = line + "+" * (2 * i -1)
+
+    line = line + "*\n"
+    graph = graph + line
+graph = graph.replace("**" ,"*")
+
+graph = graph + "*" * (n * 2 - 1)
+
+print(graph)
